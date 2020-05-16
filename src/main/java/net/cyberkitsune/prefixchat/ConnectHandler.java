6eb07c3a75f0c7474a94bff5533e92e22f183e05
Sean McClenaghan <cyberkitsune09@gmail.com>
2020-05-15 22:21:44 -0700

    Wrong command

--- src/main/java/net/cyberkitsune/prefixchat/ConnectHandler.java
+++ src/main/java/net/cyberkitsune/prefixchat/ConnectHandler.java
@@ -35,7 +35,7 @@ public class ConnectHandler implements Listener {
 			KitsuneChannel channel = KitsuneChat.getInstance().getChannelByPrefix(currentChannel);
 			if (channel != null)
 			{
-				evt.getPlayer().sendMessage(ChatColor.GRAY+String.format("[KitsuneChat] Current talking to %s. Run /kc %s to return to default.",
+				evt.getPlayer().sendMessage(ChatColor.GRAY+String.format("[KitsuneChat] Current talking to %s. Run /kc channel %s to return to default.",
 						channel.getChannelName(), KitsuneChat.getInstance().getConfig().getString("channels.default")));
 
 			}
