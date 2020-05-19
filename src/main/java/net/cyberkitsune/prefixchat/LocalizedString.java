fa9d508bf3f31ec12c51c8e893a995759a0f8bbe
CyberKitsune <cyberkitsune09@gmail.com>
2020-05-19 12:13:24 -0700

    i18n string manager

---
+++ src/main/java/net/cyberkitsune/prefixchat/LocalizedString.java
@@ -0,0 +1,59 @@
+package net.cyberkitsune.prefixchat;
+
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class LocalizedString {
+
+    public static FileConfiguration getLocaleFile()
+    {
+        InputStream inputStream = KitsuneChat.getInstance().getResource("i18n.yml");
+        if(inputStream == null)
+        {
+            KitsuneChat.getInstance().mcLog.severe("[KitsuneChat] Localization resource not found!!!");
+            return null;
+        }
+        InputStreamReader reader = new InputStreamReader(inputStream);
+
+        return YamlConfiguration.loadConfiguration(reader);
+
+    }
+
+    public static String get(String key, String locale, boolean colorized)
+    {
+        FileConfiguration lf = getLocaleFile();
+
+        if(lf == null)
+            return "";
+
+        if(!lf.contains(locale))
+            locale = "en_US";
+        String message = "";
+        if(lf.contains(locale+"."+key))
+            message = lf.getString(locale+"."+key);
+
+        if(colorized) {
+            assert message != null;
+            return ChatColor.translateAlternateColorCodes('&', message);
+        }
+        else
+            return message;
+
+    }
+
+    public static String get(String key, String locale)
+    {
+        return get(key, locale, true);
+    }
+
+    public static String get(String key)
+    {
+        return get(key, "en_US");
+    }
+
+}
